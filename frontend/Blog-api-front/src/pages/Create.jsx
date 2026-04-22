d95621107c99bde091eae66740aea9320ed2dc38
Moussa Sow <ousseynousow180@gmail.com>
2026-04-22 10:14:21 +0000

    added the details page

--- frontend/Blog-api-front/src/pages/Create.jsx
+++ frontend/Blog-api-front/src/pages/Create.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react"
 import {useNavigate} from "react-router-dom"
 import { IoMdArrowBack } from "react-icons/io";
+import toast, {Toaster} from "react-hot-toast";
 
 function Create() {
     const [title, setTitle] = useState("");
@@ -59,6 +60,7 @@ function Create() {
                     <button className="bg-black text-white cursor-pointer px-4 py-2 rounded-xl font-bold" type="submit"> Post </button>
                 </div>
             </form>
+            {/* <Toaster position="top-center"/> */}
         </section>
     )
 }
